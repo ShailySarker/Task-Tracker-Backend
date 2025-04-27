44f100cde0356312d63c3ddb62bdcc6c00257f11
ShailySarker <shailysarker99@gmail.com>
2025-04-27 18:45:50 +0600

    middleware added

---
+++ middlewares/auth.js
@@ -0,0 +1,64 @@
+const jwt = require('jsonwebtoken');
+const { verifyToken } = require('../config/jwt.js');
+const User = require('../models/User.js');
+const Project = require('../models/projectModel');
+const Task = require('../models/taskModel');
+
+const protect = async (req, res, next) => {
+    let token;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = verifyToken(token);
+
+            req.user = await User.findById(decoded.userId).select('-password');
+            next();
+        } catch (error) {
+            res.status(401);
+            throw new Error('Not authorized, token failed');
+        }
+    }
+
+    if (!token) {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+};
+
+const checkProjectOwnership = async (req, res, next) => {
+    const project = await Project.findById(req.params.projectId);
+
+    if (!project) {
+        res.status(404);
+        throw new Error('Project not found');
+    }
+
+    if (project.user.toString() !== req.user._id.toString()) {
+        res.status(403);
+        throw new Error('Not authorized to access this project');
+    }
+
+    next();
+};
+
+const checkTaskOwnership = async (req, res, next) => {
+    const task = await Task.findById(req.params.taskId);
+
+    if (!task) {
+        res.status(404);
+        throw new Error('Task not found');
+    }
+
+    if (task.user.toString() !== req.user._id.toString()) {
+        res.status(403);
+        throw new Error('Not authorized to access this task');
+    }
+
+    next();
+};
+
+module.exports = { protect, checkProjectOwnership, checkTaskOwnership };
