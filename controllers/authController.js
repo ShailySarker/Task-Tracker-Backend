b3b621800d240b62e1221b3a2e1880c7d4b49e96
ShailySarker <shailysarker99@gmail.com>
2025-04-27 18:51:20 +0600

    auth functionality completed

---
+++ controllers/authController.js
@@ -0,0 +1,63 @@
+const { generateToken } = require("../config/jwt");
+const User = require("../models/userModel");
+
+exports.registerUser = async (req, res) => {
+    const { name, email, password, country } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        country,
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            country: user.country,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+};
+
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            country: user.country,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+};
+
+exports.getUserProfile = async (req, res) => {
+    const user = await User.findById(req.user._id).select('-password');
+
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+};
